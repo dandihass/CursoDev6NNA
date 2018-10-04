5d19294f00ac5a6f160c75ab33d46613b01897f1
26147679 <26147679@UNA007-L201-015.serlab.intra>
2018-10-04 17:59:28 -0300

    Commit Final

---
+++ CursoDev6NNA/Padoca/page.js
@@ -0,0 +1,59 @@
+function AdicionarCarrinho() {
+
+	var nome = $("#produto option:selected").html();
+	var quantidade = document.getElementById('quantidade').value;
+
+	if (nome == ('Pão')) {
+		var preco = 0.5;
+	}
+	if (nome == ('Queijo')) {
+		var preco = 2;
+	}
+	if (nome == ('Café')) {
+		var preco = 1.5
+	}
+	if (nome == ('Leite')) {
+		var preco = 4;
+	}
+
+	var totalProduto = quantidade * preco;
+
+	$('#tabela').append(
+			'<tr><td>' + nome + '</td><td>' + quantidade
+					+ '</td><td name=totalProduto>' + totalProduto
+					+ '</td></tr>')
+
+	var total=0;				
+					
+	$("td[name*='totalProduto']").each(function() {
+
+		total += parseFloat($(this).html());
+		
+	})
+	
+	$('#subtotal').html(total);
+
+}
+function Unitario() {
+
+	document.getElementById('precoUnitario').innerHTML = ('');
+	var produto = document.getElementById('produto').value;
+
+	if (produto == ('pao')) {
+		document.getElementById('precoUnitario').innerHTML = ('R$0,50');
+	}
+	if (produto == ('queijo')) {
+		document.getElementById('precoUnitario').innerHTML = ('R$2,00');
+	}
+	if (produto == ('cafe')) {
+		document.getElementById('precoUnitario').innerHTML = ('R$1,50');
+	}
+	if (produto == ('leite')) {
+		document.getElementById('precoUnitario').innerHTML = ('R$4,00');
+	}
+
+	function LimparCarrinho() {
+
+	}
+
+}
